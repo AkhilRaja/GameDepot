37f2d2115b6a0906efb8bd31640938008031aa5b
Akhil Raja <p.akhilraja@gmail.com>
2020-04-12 16:31:50 +0530

    Adapter + WIP

---
+++ app/src/main/java/integration/unity/akhil/gamedepot/adapter/GamesAdapter.java
@@ -0,0 +1,25 @@
+package integration.unity.akhil.gamedepot.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GamesAdapter extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
