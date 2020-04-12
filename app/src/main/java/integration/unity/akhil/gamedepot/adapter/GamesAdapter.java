38bb65dcc57cd7c92b5b3b183416dcfc546da5e2
Akhil Raja <p.akhilraja@gmail.com>
2020-04-13 01:14:12 +0530

    Full Flow - Part 1

--- app/src/main/java/integration/unity/akhil/gamedepot/adapter/GamesAdapter.java
+++ app/src/main/java/integration/unity/akhil/gamedepot/adapter/GamesAdapter.java
@@ -1,25 +1,93 @@
 package integration.unity.akhil.gamedepot.adapter;
 
-import android.view.ViewGroup;
 
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class GamesAdapter extends RecyclerView.Adapter {
+import java.util.List;
+
+import integration.unity.akhil.gamedepot.R;
+import integration.unity.akhil.gamedepot.databinding.FragmentMainListRowItemBinding;
+import integration.unity.akhil.gamedepot.models.Result;
+import integration.unity.akhil.gamedepot.view.callback.OnClickCallBack;
+
+public class GamesAdapter extends RecyclerView.Adapter<GamesAdapter.GameViewHolder> {
+
+    public List<? extends  Result> gameList;
+
+
+    public void setGameList(final List<? extends Result> gameList) {
+        if (this.gameList == null) {
+            this.gameList = gameList;
+            notifyItemRangeInserted(0, gameList.size());
+        } else {
+            DiffUtil.DiffResult result = DiffUtil.calculateDiff(new DiffUtil.Callback() {
+                @Override
+                public int getOldListSize() {
+                    return GamesAdapter.this.gameList.size();
+                }
+
+                @Override
+                public int getNewListSize() {
+                    return gameList.size();
+                }
+
+                @Override
+                public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+                    return GamesAdapter.this.gameList.get(oldItemPosition).getName() ==
+                            GamesAdapter.this.gameList.get(newItemPosition).getName();
+                }
+
+                @Override
+                public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+                    Result newArticle = gameList.get(newItemPosition);
+                    Result oldArticle = gameList.get(oldItemPosition);
+                    return newArticle.getName().equals(oldArticle.getName())
+                            && oldArticle.getSlug().equals(newArticle.getSlug());
+                }
+            });
+            this.gameList = gameList;
+            result.dispatchUpdatesTo(this);
+        }
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public GameViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        FragmentMainListRowItemBinding binding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()),
+                R.layout.fragment_main_list_row_item,
+                parent, false);
+
+        //TODO: Add a click callback here
+        binding.setCallback(new OnClickCallBack());
+
+        return new GameViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull GameViewHolder holder, int position) {
+        //TODO: Set Games
+        holder.binding.setResult(gameList.get(position));
+        holder.binding.executePendingBindings();
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return gameList == null ? 0 : gameList.size();
+    }
+
+    public class GameViewHolder extends RecyclerView.ViewHolder {
+        FragmentMainListRowItemBinding binding;
+
+        public GameViewHolder(FragmentMainListRowItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
     }
 }
--- app/src/main/java/integration/unity/akhil/gamedepot/view/MainListFragment.java
+++ app/src/main/java/integration/unity/akhil/gamedepot/view/MainListFragment.java
@@ -2,20 +2,27 @@ package integration.unity.akhil.gamedepot.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import integration.unity.akhil.gamedepot.R;
+import integration.unity.akhil.gamedepot.adapter.GamesAdapter;
 import integration.unity.akhil.gamedepot.databinding.FragmentMainListBinding;
+import integration.unity.akhil.gamedepot.models.Games;
+import integration.unity.akhil.gamedepot.viewmodel.GameViewModel;
 
 
 public class MainListFragment extends Fragment {
 
     public static final String TAG = "ArticleListFragment";
     private FragmentMainListBinding binding;
-    //TODO: Data Binding with View and Adapter
+    private GamesAdapter gamesAdapter;
 
     public MainListFragment() {
         // Required empty public constructor
@@ -26,6 +33,39 @@ public class MainListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentMainListBinding.inflate(inflater,container,false);
+        gamesAdapter = new GamesAdapter();
+        binding.gameRV1.setAdapter(gamesAdapter);
+        binding.setIsLoading(true);
+
         return binding.getRoot();
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        GameViewModel.Factory factory = new GameViewModel.Factory(
+                getActivity().getApplication());
+
+        final GameViewModel viewModel = ViewModelProviders.of(this, factory)
+                .get(GameViewModel.class);
+
+        binding.setIsLoading(true);
+
+        observeViewModel(viewModel);
+    }
+
+    private void observeViewModel(GameViewModel viewModel) {
+        // Update the list when the data changes
+        viewModel.getObservableProject().observe(getViewLifecycleOwner(), new Observer<Games>() {
+            @Override
+            public void onChanged(@Nullable Games games) {
+                if (games!= null) {
+                    binding.setIsLoading(false);
+                    gamesAdapter.setGameList(games.getResults());
+                }
+            }
+        });
     }
 }
